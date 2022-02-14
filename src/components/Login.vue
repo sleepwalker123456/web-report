cb0f95b67265e4ee02225f3a51585b844ce18ef6
sleepwalker <406715003@qq.com>
2022-02-14 22:12:33 +0800

    abc

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>登录组件</div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-const Home = () => import('../views/home/Home')
-const Cart = () => import('../views/cart/Cart')
-const Profile = () => import('../views/profile/Profile')
-const Category = () => import('../views/category/Category')
+const Login = () => import('components/Login')
+const Home = () => import('views/home/Home')
+const Cart = () => import('views/cart/Cart')
+const Profile = () => import('views/profile/Profile')
+const Category = () => import('views/category/Category')
 
 Vue.use(Router)
 
 const routes = [
+  {
+    path: '',
+    component: Login
+  },
   {
     path: '/home',
     component: Home
